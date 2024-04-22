a8cef3e6fe85ee11879e0a3fc48fe25ba1d55399
ferraridk <henrikferrari@gmail.com>
2024-04-22 18:20:24 +0200

    Models, dummy data and listview

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -10,6 +12,24 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Computer',
+        amount: 12000.00,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Italy',
+        amount: 19300.50,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        title: 'Cinema',
+        amount: 186.00,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
